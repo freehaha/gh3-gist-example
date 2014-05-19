dbf37ae3f378cdca045df227214afad48f240320
Freehaha <freehaha@gmail.com>
2014-05-19 14:20:15 +1000

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+angular.module('githubEx', [])
+  .controller('MainCtrl', ['$scope',
+    function($scope) {
+      $scope.gistId = '4992826';
+      $scope.result = '';
+      $scope.files = [];
+      $scope.getFiles = function() {
+        var gist = new Gh3.Gist({id: $scope.gistId});
+        $scope.files = [];
+        gist.fetchContents(function(err, res) {
+          if(err) {
+            $scope.$apply(function() {
+              $scope.result = err.message;
+            });
+          } else {
+            $scope.$apply(function() {
+              $scope.result = '';
+            });
+          }
+          gist.eachFile(function(file) {
+            $scope.$apply(function() {
+              $scope.files.push(file);
+            });
+          });
+        });
+      };
+    }
+  ]);
